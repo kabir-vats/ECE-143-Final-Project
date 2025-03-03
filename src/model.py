703650a5ac631a1b51d323055a9d2e7466118200
Lyttr <jgy2001@126.com>
2025-03-03 07:15:27 -0800

    update models

--- src/model.py
+++ src/model.py
@@ -11,6 +11,12 @@ from sklearn.metrics import accuracy_score
 
 
 class TextClassifier:
+    """Classifier with basic tokenizers and models
+
+    Tokenizers : TF-IDF, Count
+    
+    Models : naive_bayes, logistic_regression, svm, random_forest, knn
+    """    
     models = {
             "naive_bayes": MultinomialNB(),
             "logistic_regression": LogisticRegression(max_iter=1000),
@@ -23,7 +29,16 @@ class TextClassifier:
             "count": CountVectorizer(),
         }
     def __init__(self, model_name="naive_bayes", tokenizer_name="tfidf"):
-        
+        """Classifier with basic tokenizers and models
+
+        Tokenizers : TF-IDF, Count
+    
+        Models : naive_bayes, logistic_regression, svm, random_forest, knn
+
+        Args:
+            model_name (str, optional): Defaults to "naive_bayes".
+            tokenizer_name (str, optional): Defaults to "tfidf".
+        """        
         self.vectorizer = self._get_tokenizer(tokenizer_name)
         self.model = self._get_model(model_name)
         self.pipeline = make_pipeline(self.vectorizer, self.model)
@@ -33,8 +48,6 @@ class TextClassifier:
         return TextClassifier.models[model_name]
 
     def _get_tokenizer(self, tokenizer_name):
-      
-        
         assert tokenizer_name in TextClassifier.tokenizers, f"Tokenizer '{tokenizer_name}' is not supported. Choose from {list(TextClassifier.tokenizers.keys())}."
         return TextClassifier.tokenizers[tokenizer_name]
     
@@ -42,15 +55,35 @@ class TextClassifier:
         return f'Vectorizer : {self.vectorizer}, Model : {self.model}'
     
     def train(self, X_train, y_train):
-      
+        """Train models
+
+        Args:
+            X_train : Train data
+            y_train : Labels
+        """        
         self.pipeline.fit(X_train, y_train)
 
     def predict(self, X_test):
-       
+        """Predict with provided data
+
+        Args:
+            X_test : data to predict
+
+        Returns:
+            _type_: Prediction results
+        """        
         return self.pipeline.predict(X_test)
 
     def evaluate(self, X_test, y_test):
-       
+        """Evaluate model with test data
+
+        Args:
+            X_test (_type_): Test data
+            y_test (_type_): Test data labels
+
+        Returns:
+            Float: acc score
+        """       
         y_pred = self.predict(X_test)
         return accuracy_score(y_test, y_pred)
 

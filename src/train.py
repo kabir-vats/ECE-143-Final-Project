e3173f9a3c58a208e40e4005a857e1c5c189155a
Lyttr <jgy2001@126.com>
2025-03-03 20:26:58 -0800

    init train.py and evaluation.py

---
+++ src/train.py
@@ -0,0 +1,7 @@
+import argparse
+import joblib
+from sklearn.metrics import classification_report
+from data_loader import  dataset_split
+from model import TextClassifier
+
+def main(model_name, tokenizer_name, save_path):

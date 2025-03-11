8cf6e1533bfda8a6ce126115822e894606d04622
kabir-vats <kabir.vats.2004@gmail.com>
2025-03-11 14:18:04 -0700

    drop duplicates in preprocess

--- src/preprocess.py
+++ src/preprocess.py
@@ -7,13 +7,15 @@ def preprocess_raw(new_path='../raw'):
         new_path (str, optional): path to save cleanned files. Defaults to 'raw'.
     """    
     path=data_loader.download()
-    fake_raw = pd.read_csv(path+'/Fake.csv', keep_default_na=False) 
+    fake_raw = pd.read_csv(path+'/Fake.csv', keep_default_na=False)
     fake_raw['text'] = fake_raw['text'].astype(str).str.strip() 
     fake_raw['text'] = fake_raw['text'].replace(r'^\s*$', float('nan'), regex=True)  #
     fake_raw.dropna(subset=["text"], inplace=True) 
+    fake_raw.drop_duplicates(inplace=True) 
     fake_raw.to_csv(new_path+'/Fake.csv', index=False)
     true_raw = pd.read_csv(path+'/True.csv', keep_default_na=False)
     true_raw['text'] = true_raw['text'].astype(str).str.strip()
     true_raw['text'] = true_raw['text'].replace(r'^\s*$', float('nan'), regex=True)
     true_raw.dropna(subset=["text"], inplace=True)
+    true_raw.drop_duplicates(inplace=True)
     true_raw.to_csv(new_path+'/True.csv', index=False)

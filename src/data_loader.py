8d292295d3bc075a11bc3672b0ad9e3b04e25c36
kabir-vats <kabir.vats.2004@gmail.com>
2025-03-12 16:23:34 -0700

    update dataset split in loader to trim

--- src/data_loader.py
+++ src/data_loader.py
@@ -11,6 +11,23 @@ def download()->str:
     path = kagglehub.dataset_download("clmentbisaillon/fake-and-real-news-dataset")
     return path
 
+
+def trim_string(x, max_len=200) -> str:
+    """Trim string to max_len words
+
+    Args:
+        x (str): string to trim
+        max_len (int, optional): maximum number of words to keep. Defaults to 200.
+    
+    Returns:
+        str: trimmed string
+    """
+    x = x.split(maxsplit=max_len)
+    x = ' '.join(x[:max_len])
+
+    return x
+
+
 def dataset_split(path='raw/',ratio=(0.7,0.15,0.15))->pd.DataFrame:
     """split raw csv files into train, validation and test sets
 
@@ -19,15 +36,19 @@ def dataset_split(path='raw/',ratio=(0.7,0.15,0.15))->pd.DataFrame:
         ratio (tuple, optional): splitting ratio. Defaults to (0.7,0.15,0.15).
 
     Returns:
-        pd.DataFrame: train, validation and test sets
-    """    
+        tuple: Contains (train_df, val_df, test_df) as pandas DataFrames
+    """
     assert sum(ratio)==1.0 and len(ratio)==3, "ratio error"
-    true_df = pd.read_csv(path+'Fake.csv')
-    fake_df = pd.read_csv(path+'True.csv')
+    true_df = pd.read_csv(path+'True.csv')
+    fake_df = pd.read_csv(path+'Fake.csv')
     true_df["label"] = 1
     fake_df["label"] = 0
     df = pd.concat([true_df, fake_df], ignore_index=True)
+    df['titletext'] = df['title'] + ". " + df['text']
+    df['text'] = df['text'].apply(trim_string)
+    df['titletext'] = df['titletext'].apply(trim_string)    
+    df = df.reindex(columns=['label', 'title', 'text', 'titletext'])
     df = df.sample(frac=1, random_state=42).reset_index(drop=True)
     train_df, temp_df = train_test_split(df, test_size=ratio[2]+ratio[1], random_state=42, stratify=df["label"])
     val_df, test_df = train_test_split(temp_df, test_size=ratio[2]/(ratio[2]+ratio[1]), random_state=42, stratify=temp_df["label"])
-    return train_df,val_df,test_df
+    return train_df, val_df, test_df
